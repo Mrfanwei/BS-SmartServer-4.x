11564d177ccfa56139d99742149475fe67a38e83
Mrfanwei <546271337@qq.com>
2017-07-18 15:10:46 +0800

    qintinfm能在ubuntu上运行 注：打包jar包时应该在qintin目录下用mvn package

--- qintin/src/main/java/com/smartlife/QintinApplication.java
+++ qintin/src/main/java/com/smartlife/QintinApplication.java
@@ -1,9 +1,11 @@
 package com.smartlife;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.smartlife.repositories")
 public class QintinApplication {
 
 	public static void main(String[] args) {
